e2191318500a2559a13829139c6b1688befe98d9
rdalla <rodvfilho@terra.com.br>
2020-07-12 21:02:25 -0300

    Changed the README.md to explain the main function of the python script.
    Changed the way to save the data, instead of JSON file, it will be the InfluxDB.
    
    Added the influxdb configuration and the mechanism to get the MQTT message, parse it and save it to influx database.
    
    Renamed the python script.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ##The main features:
 
 - The Broker receives information of all the clients that published on the topic of energy monitoring.
-- The Python Script is responsible to get the subscribed message and save in a JSON.
+- The Python Script is responsible to get the subscribed message and save in a database.
---
+++ src/beaglemqttinflux.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+"""A MQTT Broker
+This script receives MQTT data and saves those to JSON file.
+"""
+import re
+from typing import NamedTuple
+
+import datetime
+
+import paho.mqtt.client as mqtt
+from influxdb import InfluxDBClient
+
+
+# mqtt configuration
+MQTT_ADDRESS = '192.168.1.101'
+MQTT_USER = 'rdalla'
+MQTT_PASSWORD = 'vao1ca'
+#MQTT_TOPIC = '/home'
+MQTT_TOPIC = 'home/+/+' # [sensor]/[irms|kwh|cost]
+MQTT_REGEX = 'home/([^/]+)/([^/]+)'
+MQTT_CLIENT_ID = 'DallaValleMQTTBroker'
+
+# influx configuration
+ifuser = "grafana"
+ifpass = "dallavalle"
+ifdb   = "home"
+ifhost = "192.168.1.101"
+ifport = 8086
+measurement_name = "home_power"
+
+# connect to influx
+ifclient = InfluxDBClient(ifhost,ifport,ifuser,ifpass,ifdb)
+
+# take a timestamp for this measurement
+time = datetime.datetime.utcnow()
+
+class SensorData(NamedTuple):
+    location: str
+    measurement: str
+    value: float
+
+def on_connect(client, userdata, flags, rc):
+    """ The callback for when the client receives a CONNACK response from the server."""
+    print('Connected with result code ' + str(rc))
+    client.subscribe(MQTT_TOPIC)
+
+def on_message(client, userdata, msg):
+    """The callback for when a PUBLISH message is received from the server."""
+    print(msg.topic + ' ' + str(msg.payload))
+    sensor_data = _parse_mqtt_message(msg.topic, msg.payload.decode('utf-8'))
+    if sensor_data is not None:
+        _send_sensor_data_to_influxdb(sensor_data)
+
+def _parse_mqtt_message(topic, payload):
+    match = re.match(MQTT_REGEX, topic)
+    if match:
+        location = match.group(1)
+        measurement = match.group(2)
+        return SensorData(location, measurement, float(payload))
+    else:
+        return None
+
+# format the data as a single measurement for influx
+def _send_sensor_data_to_influxdb(sensor_data):
+    json_body = [
+        {
+            'measurement': measurement_name,
+            'time':time,
+            'tags': {
+                'location': sensor_data.measurement
+            },
+            'fields': {
+                'value': sensor_data.value
+            }
+        }
+    ]
+    ifclient.write_points(json_body)
+
+
+def main():
+    mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
+    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    mqtt_client.connect(MQTT_ADDRESS, 1883)
+    mqtt_client.loop_forever()
+
+
+if __name__ == '__main__':
+    print('DallaValle MQTT Broker')
+    main()
