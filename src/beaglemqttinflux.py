3aa04b6bbd89a45fb69de48bf50104386bf80ca1
rdalla <rodvfilho@terra.com.br>
2020-07-19 13:22:54 -0300

    Changed the measurement name to change the base of my influx database, because it was implemented a sum of kWh and cost in my sensor client.

--- src/beaglemqttinflux.py
+++ src/beaglemqttinflux.py
@@ -26,7 +26,7 @@ ifpass = "dallavalle"
 ifdb   = "home"
 ifhost = "192.168.1.101"
 ifport = 8086
-measurement_name = "power_meter"
+measurement_name = "smart_meter"
 
 # connect to influx
 ifclient = InfluxDBClient(ifhost,ifport,ifuser,ifpass,ifdb)
